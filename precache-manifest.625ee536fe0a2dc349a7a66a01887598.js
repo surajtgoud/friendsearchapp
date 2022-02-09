1d7f22019854b3e08110dda9fd5bc878a1330b67
Surajgoud <surajgoud@Surajgouds-MacBook-Pro.local>
2022-02-09 20:44:57 +0530

    Updates

---
+++ precache-manifest.625ee536fe0a2dc349a7a66a01887598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe3ee14651143e0b534a347f685d209",
+    "url": "/friendsearchapp/index.html"
+  },
+  {
+    "revision": "590b2a58f38c125bde60",
+    "url": "/friendsearchapp/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "465db40cc36c0d43e3f0",
+    "url": "/friendsearchapp/static/css/main.03e6d29f.chunk.css"
+  },
+  {
+    "revision": "590b2a58f38c125bde60",
+    "url": "/friendsearchapp/static/js/2.9858653f.chunk.js"
+  },
+  {
+    "revision": "465db40cc36c0d43e3f0",
+    "url": "/friendsearchapp/static/js/main.0fa24a55.chunk.js"
+  },
+  {
+    "revision": "217bc9f290fd7826f578",
+    "url": "/friendsearchapp/static/js/runtime~main.5a02ff91.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friendsearchapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
